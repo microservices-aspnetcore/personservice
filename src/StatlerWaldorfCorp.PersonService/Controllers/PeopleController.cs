e9877a153c04189e0465a2cf0037363b5ac192d5
Chris Umbel <chris@chrisumbel.com>
2016-07-05 17:36:56 +0000

    started PeopleController

---
+++ src/StatlerWaldorfCorp.PersonService/Controllers/PeopleController.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StatlerWaldorfCorp.PersonService
+{
+	[Route("[controller]")]
+	public class PeopleController : Controller
+	{
+		[HttpGet]
+        public async virtual Task<IActionResult> GetAllPeople()
+		{
+			return this.Ok("person");
+		}
+	}
+}
